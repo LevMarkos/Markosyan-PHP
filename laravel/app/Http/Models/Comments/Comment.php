72abc7a200349c2670d1430d932df05118adc806
LevMarkos <levmarcosyan@mail.ru>
2025-04-29 16:33:08 +0300

    Initial commit

--- laravel/app/Http/Models/Comments/Comment.php
+++ laravel/app/Http/Models/Comments/Comment.php
@@ -1,11 +1,20 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 
 class Comment extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['post_id', 'content'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
     protected static function boot()
     {
         parent::boot();
--- laravel/app/Http/Models/Users/User.php
+++ laravel/app/Http/Models/Users/User.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 
 class User extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['name', 'email'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
     protected static function boot()
     {
         parent::boot();
