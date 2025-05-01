91e52a0d3e8f5d1e415fc44390f16b9b092b37fa
LevMarkos <levmarcosyan@mail.ru>
2025-05-01 12:02:15 +0300

    Initial commit

--- laravel/app/Http/Models/Users/User.php
+++ laravel/app/Http/Models/Users/User.php
@@ -2,34 +2,47 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'email'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
-    public function posts()
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return $this->hasMany(Post::class);
-    }
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::created(function ($user) {
-            Log::channel('sevenchangeschanges')->info('Пользователь создан: ', $user->toArray());
-        });
-
-        static::updated(function ($user) {
-            Log::channel('sevenchangeschanges')->info('Пользователь обновлен: ', $user->toArray());
-        });
-
-        static::deleted(function ($user) {
-            Log::channel('sevenchangeschanges')->info('Пользователь удален: ', $user->toArray());
-        });
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
