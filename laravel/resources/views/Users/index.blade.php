a7fa939749885f8f497473f3c583d597d25994a2
LevMarkos <levmarcosyan@mail.ru>
2025-05-01 17:49:36 +0300

    Initial commit

--- laravel/resources/views/Users/index.blade.php
+++ laravel/resources/views/Users/index.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Список пользователей</h1>
-    <a href="{{ route('users.create') }}">Создать пользователя</a>
-    <ul>
-        @foreach($users as $user)
-            <li>{{ $user->name }} - <a href="{{ route('users.edit', $user->id) }}">Редактировать</a></li>
-        @endforeach
-    </ul>
+<h1>Список постов</h1>
+
+@if(auth()->user()->role === 'admin' || auth()->user()->role === 'editor')
+    <a href="{{ route('posts.create') }}">Создать пост</a>
+@endif
+
+<ul>
+@foreach($posts as $post)
+    <li>
+        <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+        @if(auth()->user()->role === 'admin' || auth()->user()->role === 'editor')
+            - <a href="{{ route('posts.edit', $post->id) }}">Редактировать</a>
+        @endif
+    </li>
+@endforeach
+</ul>
+
+@if(auth()->user()->role === 'user' || auth()->user()->role === 'editor' || auth()->user()->role === 'admin')
+    <h2>Добавить комментарий</h2>
+@endif
 @endsection
