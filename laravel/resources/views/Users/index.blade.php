4f070aaa2c9e2a0260a168a8efbb421a79ea9974
LevMarkos <levmarcosyan@mail.ru>
2025-05-06 13:12:34 +0300

    Initial commit

--- laravel/resources/views/Users/index.blade.php
+++ laravel/resources/views/Users/index.blade.php
@@ -1,26 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-
 <h1>Список постов</h1>
 
-@can('create', App\Models\Post::class)
+@if(auth()->user()->isAdmin() || auth()->user()->isEditor())
     <a href="{{ route('posts.create') }}">Создать пост</a>
-@endcan
-
-<ul>
-    @foreach($posts as $post)
-        <li>
-            <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
-            @can('update', $post)
-                - <a href="{{ route('posts.edit', $post->id) }}">Редактировать</a>
-            @endcan
-        </li>
-    @endforeach
-</ul>
-
-@if(auth()->user()->can('comment', App\Models\Post::class))
-    <h2>Добавить комментарий</h2>
 @endif
 
+<ul>
+@foreach($posts as $post)
+    <li>
+        <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+        @if(auth()->user()->isAdmin() || auth()->user()->isEditor())
+            - <a href="{{ route('posts.edit', $post->id) }}">Редактировать</a>
+        @endif
+    </li>
+@endforeach
+</ul>
+
+@if(auth()->user()->isUser () || auth()->user()->isEditor() || auth()->user()->isAdmin())
+    <h2>Добавить комментарий</h2>
+@endif
 @endsection
+
